dfc573502f65f025666d6592fbcc0abdfa6a160c
Mir Ahmad Ali <mali3@ecc-jaguar.csuchico.edu>
2019-09-09 17:11:07 -0700

    The change in entries

--- madlibs.cpp
+++ madlibs.cpp
@@ -2,8 +2,29 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
 int main()
 {
-
+  string Firstname;
+   cin>>Firstname;
+  int number;
+   cin>>number;
+  string Adjective;
+   cin>>Adjective;
+  string Drink;
+   cin>>Drink;
+  float decimal;
+   cin>>decimal;
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
